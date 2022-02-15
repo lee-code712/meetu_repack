880c09c94cdf60ca11e9a959844f3b2f2af9aed4
yuriLee <yurilee07@naver.com>
2022-02-15 17:07:57 +0900

    상담가능시간dto 추가

---
+++ src/main/java/project/meetu/model/dto/ConsultableTime.java
@@ -0,0 +1,27 @@
+package project.meetu.model.dto;
+
+public class ConsultableTime {
+	private int able_date;
+	private String able_time;
+	private String prof_id;
+	
+	public int getAble_date() {
+		return able_date;
+	}
+	public void setAble_date(int able_date) {
+		this.able_date = able_date;
+	}
+	public String getAble_time() {
+		return able_time;
+	}
+	public void setAble_time(String able_time) {
+		this.able_time = able_time;
+	}
+	public String getProf_id() {
+		return prof_id;
+	}
+	public void setProf_id(String prof_id) {
+		this.prof_id = prof_id;
+	}
+	
+}
