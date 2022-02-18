9ceaae6a57830cfb213d032f6f3d22f80bbee177
yuriLee <yurilee07@naver.com>
2022-02-18 22:08:50 +0900

    상담일지 출력 구현

--- src/main/java/project/meetu/controller/consult/ConsultRecordController.java
+++ src/main/java/project/meetu/controller/consult/ConsultRecordController.java
@@ -1,15 +1,28 @@
 package project.meetu.controller.consult;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import project.meetu.model.dto.Consult;
+import project.meetu.model.service.ConsultManager;
+
 @Controller
 public class ConsultRecordController {
+	
+	private final ConsultManager consultService;
+	
+	@Autowired
+	public ConsultRecordController(ConsultManager consultService) {
+		this.consultService = consultService;
+	}
 
 	@GetMapping("/consult/recordConsult")
-	public String goConsultRecordForm() {
-
+	public String goConsultRecordForm(String consultId, Model model) {
+		
+		Consult consult = consultService.getReservationInfo(consultId);
+		model.addAttribute("consult", consult);
 		
 		return "consult/consultRecordForm";
 	}
--- src/main/java/project/meetu/controller/consult/ProfessorSearchController.java
+++ src/main/java/project/meetu/controller/consult/ProfessorSearchController.java
@@ -3,24 +3,16 @@ package project.meetu.controller.consult;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import project.meetu.model.dto.College;
-import project.meetu.model.dto.Consult;
-import project.meetu.model.dto.Course;
 import project.meetu.model.dto.Department;
 import project.meetu.model.dto.Professor;
-import project.meetu.model.service.ConsultManager;
 import project.meetu.model.service.UserManager;
 
 @Controller
@@ -35,9 +27,6 @@ public class ProfessorSearchController {
 	
 	@GetMapping(value = "/consult/professorSearch")
 	public String goProfessorSearchPage(HttpServletRequest req, Model model) {
-		
-		HttpSession session = req.getSession();
-		String userId = (String) session.getAttribute("id");
 		
 		List<College> collegeList = userService.getColleges();
 		if (collegeList != null) {
@@ -62,6 +51,7 @@ public class ProfessorSearchController {
 	
 	@GetMapping(value = "/consult/professorSearchByDeptNo")
 	public ModelAndView ProfessorSearch(HttpServletRequest req, ModelAndView mav) {
+		
 		String deptNo = (String) req.getParameter("deptNo");
 		
 		List<Professor> professorList = userService.getDeptProfessors(deptNo);
