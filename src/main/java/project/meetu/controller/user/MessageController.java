fd910fdb06afc1a5fdc3277623ff90147ea0374b
hr_0513 <hr_0513@naver.com>
2022-02-18 16:43:38 +0900

    message 추가

---
+++ src/main/java/project/meetu/controller/user/MessageController.java
@@ -0,0 +1,14 @@
+package project.meetu.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+	
+	@GetMapping("/user/message")
+	public String goLoginForm() {
+		return "user/message";
+	}
+
+}
