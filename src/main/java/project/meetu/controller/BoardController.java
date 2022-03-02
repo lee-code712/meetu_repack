b131be873e042d9d3e1cd7545dfd918de4015131
hr_0513 <hr_0513@naver.com>
2022-03-02 17:27:28 +0900

    공지사항 추가

---
+++ src/main/java/project/meetu/controller/BoardController.java
@@ -0,0 +1,12 @@
+package project.meetu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	@GetMapping("/board")
+	public String goMessagePage() {
+		return "board";
+	}
+}
